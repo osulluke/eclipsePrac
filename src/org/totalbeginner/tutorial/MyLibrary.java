be8d6d09fcdbd2ded5ded304215b9d655850b3e4
Luke O'Sullivan <osulluke@gmail.com>
2016-10-02 17:46:50 -0700

    Lesson 12, creation of checkOut and checkIn methods.
    Here is where I've implemented the checkOut and checkIn methods in the MyLibrary class. Here, it's important to notice that these methods return True or False values, which enhances their ability to be tested in the MyLibraryTest class. I am using at least one method that the instructor hasn't shown yet, that I'm not sure will work. This is the 'contains' method to see if the book exists. I think it's a good idea to make sure a book exists before I check it out...though, adimittedly, I'm not sure if this will work off the bat...we'll have to see. The other part that I got wrong initially was I was using an empty string to test the getName() method against - this might have worked, but I decided to just go with the 'null' as recommended by the instructor.

--- src/org/totalbeginner/tutorial/MyLibrary.java
+++ src/org/totalbeginner/tutorial/MyLibrary.java
@@ -3,13 +3,11 @@ package org.totalbeginner.tutorial;
 import java.util.ArrayList;
 
 public class MyLibrary {
-
 	String name;
 	ArrayList<Book> books;
 	ArrayList<Person> people;
 
 	public MyLibrary(String name) {
-		// TODO Auto-generated constructor stub
 		this.name = name;
 		this.books = new ArrayList<Book>();
 		this.people = new ArrayList<Person>();
@@ -28,23 +26,41 @@ public class MyLibrary {
 	}
 
 	public void addBook(Book b1) {
-		// TODO Auto-generated method stub
 		this.books.add(b1);
 	}
 
 	public void addPerson(Person p1) {
-		// TODO Auto-generated method stub
 		this.people.add(p1);
 	}
 
 	public void removeBook(Book b1) {
-		// TODO Auto-generated method stub
 		this.books.remove(b1);
 	}
 
 	public void removePerson(Person p1) {
-		// TODO Auto-generated method stub
 		this.people.remove(p1);
+	}
+
+	public boolean checkOut(Book b1, Person p1) {
+
+		if(b1.getPerson().getName() == null && books.contains(b1) == true){
+			b1.person = p1;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public boolean checkIn(Book b1) {
+		// TODO Auto-generated method stub
+		if (b1.getPerson().getName() == null){
+			return false;
+		}
+		else {
+			b1.person = null;
+			return true;
+		}
 	}
 
 }
